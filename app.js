85668d5e96127b4d824953a35cdeed830071c1f6
Stenoi <stenoner90@gmail.com>
2021-08-20 23:43:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const routes = require('./routes/cedimagen')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    next()
+})
+
+app.use('/', routes)
+
+module.exports = app
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const app = require('./app')
+const port = 3700
+const server = 'localhost'
+const db = 'twitter-clone'
+const mongodb = `mongodb://${server}:27017/${db}`
+
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useCreateIndex', true)
+mongoose.Promise = global.Promise
+mongoose.connect(mongodb)
+    .then(() => {
+        console.log('Conexion a mongodb establecida.')
+        app.listen(port, () => {
+            console.log(`Escuchando desde el puerto: ${port}`)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
