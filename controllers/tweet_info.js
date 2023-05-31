4d8aa6cd2caf12f0123c19fb74278653efe11852
Leonardo Acevedo <stenoner90@gmail.com>
2023-05-30 19:20:55 -0500

    added comment likes and media to request

--- controllers/tweet_info.js
+++ controllers/tweet_info.js
@@ -5,6 +5,8 @@ const TweetBookmark = require('../models/tweet_bookmark')
 const TweetComment = require('../models/tweet_comment')
 const TweetLike = require('../models/tweet_like')
 const TweetMediaContent = require('../models/tweet_mediacontent')
+const TweetCommentMediaContent = require('../models/tweet_comment_mediacontent')
+const TweetCommentLike = require('../models/tweet_comment_like')
 const TweetRetweet = require('../models/tweet_retweet')
 const ProfileFollow = require('../models/profile_follow')
 
@@ -26,6 +28,26 @@ const controller = {
         const comments = await TweetComment.find({ tweetID })
             .populate([{ path: 'tweetID' }, { path: 'profileID' }])
             .exec()
+        const commentsInfo = await Promise.all(
+            comments.map(async (comment) => {
+                const commentMediaContents = await TweetCommentMediaContent.find({ tweetCommentID: comment._id })
+                    .exec()
+                const commentLikes = await TweetCommentLike.find({ tweetCommentID: comment._id })
+                    .populate([{ path: 'tweetCommentID' }, { path: 'profileID' }])
+                    .sort({ createdAt: 'desc' })
+                    .exec()
+                return {
+                    _id: comment._id,
+                    content: comment.content,
+                    createdAt: comment.createdAt,
+                    profileID: comment.profileID,
+                    state: comment.state,
+                    tweetID: comment.tweetID,
+                    likes: commentLikes,
+                    mediaContents: commentMediaContents,
+                }
+            })
+        )
         const likes = await TweetLike.find({ tweetID })
             .populate([{ path: 'tweetID' }, { path: 'profileID' }])
             .exec()
@@ -41,7 +63,7 @@ const controller = {
             profleID: tweet.profileID,
             tweetReplyOptionID: tweet.tweetReplyOptionID,
             bookmarks,
-            comments,
+            comments: commentsInfo,
             likes,
             mediaContents,
             retweets,
@@ -70,6 +92,26 @@ const controller = {
                 const comments = await TweetComment.find({ tweetID: tweet._id })
                     .populate([{ path: 'tweetID' }, { path: 'profileID' }])
                     .exec()
+                const commentsInfo = await Promise.all(
+                    comments.map(async (comment) => {
+                        const commentMediaContents = await TweetCommentMediaContent.find({ tweetCommentID: comment._id })
+                            .exec()
+                        const commentLikes = await TweetCommentLike.find({ tweetCommentID: comment._id })
+                            .populate([{ path: 'tweetCommentID' }, { path: 'profileID' }])
+                            .sort({ createdAt: 'desc' })
+                            .exec()
+                        return {
+                            _id: comment._id,
+                            content: comment.content,
+                            createdAt: comment.createdAt,
+                            profileID: comment.profileID,
+                            state: comment.state,
+                            tweetID: comment.tweetID,
+                            likes: commentLikes,
+                            mediaContents: commentMediaContents,
+                        }
+                    })
+                )
                 const likes = await TweetLike.find({ tweetID: tweet._id })
                     .populate([{ path: 'tweetID' }, { path: 'profileID' }])
                     .exec()
@@ -86,7 +128,7 @@ const controller = {
                     tweetReplyOptionID: tweet.tweetReplyOptionID,
                     content: tweet.content,
                     bookmarks,
-                    comments,
+                    comments: commentsInfo,
                     likes,
                     mediaContents,
                     retweets,
