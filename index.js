c20d2a977a78910387ecbdae7e8656804ca668bc
Leonardo Acevedo <stenoner90@gmail.com>
2023-06-20 12:04:41 -0500

    remote connection string for mongo

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict'
 
+require('dotenv').config()
 const mongoose = require('mongoose')
 const app = require('./app')
 const port = 3700
-const server = '127.0.0.1'
-const db = 'twitter-clone'
-const mongodb = `mongodb://${server}:27017/${db}`
+const user = process.env.MONGO_USER
+const password = process.env.MONGO_PASSWORD
+const database = process.env.MONGO_DATABASE
+const mongo_connectionstring = `mongodb+srv://${user}:${password}@cluster0.ikegkll.mongodb.net/${database}`
 
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
 mongoose.set('useUnifiedTopology', true)
 mongoose.set('useCreateIndex', true)
 mongoose.Promise = global.Promise
-mongoose.connect(mongodb)
+mongoose.connect(mongo_connectionstring)
     .then(() => {
         console.log('Conexion a mongodb establecida.')
         app.listen(port, () => {
