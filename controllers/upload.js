6b6842e221e973515fc25a7100d07ec8c9ab757f
Leonardo Acevedo <stenoner90@gmail.com>
2023-07-06 16:26:26 -0500

    added cloudinary when local write is not available

--- controllers/upload.js
+++ controllers/upload.js
@@ -8,47 +8,47 @@ const helper = require('../helpers/helper')
 const fs = require('fs')
 
 const controller = {
-    uploadProfilePicture: (req, res) => {
-        const body = req.body
-        const file = req.file
-        const profileID = body.profileID
-        Profile.findByIdAndUpdate(profileID, { picture: file.filename }, { new: false }, (err, profileSuccess) => {
-            if (!profileSuccess) return res.status(400).send({ message: 'No se encontro el perfil.' })
-            if (err) return res.status(500).send({ message: 'No se pudo resolver la peticion.' })
-            console.log(`${helper.getFullTime()} - Se empezo a subir la foto de perfil ${file.filename}...`)
-            return res.status(200).send({ message: 'La foto de perfil se subio exitosamente.' })
-        })
-    },
     uploadProfileBanner: (req, res) => {
         const body = req.body
-        const file = req.file
+        const file = req.cloudinary_url ?? req.file.filename
         const profileID = body.profileID
-        Profile.findByIdAndUpdate(profileID, { banner: file.filename }, { new: false }, (err, profileSuccess) => {
+        Profile.findByIdAndUpdate(profileID, { banner: file }, { new: false }, (err, profileSuccess) => {
             if (!profileSuccess) return res.status(400).send({ message: 'No se encontro el perfil.' })
             if (err) return res.status(500).send({ message: 'No se pudo resolver la peticion.' })
-            console.log(`${helper.getFullTime()} - Se empezo a subir el banner de perfil ${file.filename}...`)
+            console.log(`${helper.getFullTime()} - Se empezo a subir el banner de perfil ${file}...`)
             return res.status(200).send({ message: 'El banner de perfil se subio exitosamente.' })
+        })
+    },
+    uploadProfilePicture: (req, res) => {
+        const body = req.body
+        const file = req.cloudinary_url ?? req.file.filename
+        const profileID = body.profileID
+        Profile.findByIdAndUpdate(profileID, { picture: file }, { new: false }, (err, profileSuccess) => {
+            if (!profileSuccess) return res.status(400).send({ message: 'No se encontro el perfil.' })
+            if (err) return res.status(500).send({ message: 'No se pudo resolver la peticion.' })
+            console.log(`${helper.getFullTime()} - Se empezo a subir la foto de perfil ${file}...`)
+            return res.status(200).send({ message: 'La foto de perfil se subio exitosamente.' })
         })
     },
     uploadTweetImage: (req, res) => {
         const body = req.body
-        const file = req.file
+        const file = req.cloudinary_url ?? req.file.filename
         const tweetMediaContentID = body.tweetMediaContentID
-        TweetMediaContent.findByIdAndUpdate(tweetMediaContentID, { content: file.filename }, { new: false }, (err, tweetMediaContentSuccess) => {
+        TweetMediaContent.findByIdAndUpdate(tweetMediaContentID, { content: file }, { new: false }, (err, tweetMediaContentSuccess) => {
             if (!tweetMediaContentSuccess) return res.status(400).send({ message: 'No se encontro el contenido de media para el tweet.' })
             if (err) return res.status(500).send({ message: 'No se pudo resolver la peticion.' })
-            console.log(`${helper.getFullTime()} - Se empezo a subir imagen de tweet ${file.filename}...`)
+            console.log(`${helper.getFullTime()} - Se empezo a subir imagen de tweet ${file}...`)
             return res.status(200).send({ message: 'El contenido de media se subio exitosamente.' })
         })
     },
     uploadTweetCommentImage: (req, res) => {
         const body = req.body
-        const file = req.file
+        const file = req.cloudinary_url ?? req.file.filename
         const tweetCommentMediaContentID = body.tweetCommentMediaContentID
-        TweetCommentMediaContent.findByIdAndUpdate(tweetCommentMediaContentID, { content: file.filename }, { new: false }, (err, tweetCommentMediaContentSuccess) => {
+        TweetCommentMediaContent.findByIdAndUpdate(tweetCommentMediaContentID, { content: file }, { new: false }, (err, tweetCommentMediaContentSuccess) => {
             if (!tweetCommentMediaContentSuccess) return res.status(400).send({ message: 'No se encontro el contenido de media para el tweet.' })
             if (err) return res.status(500).send({ message: 'No se pudo resolver la peticion.' })
-            console.log(`${helper.getFullTime()} - Se empezo a subir imagen de comentario ${file.filename}...`)
+            console.log(`${helper.getFullTime()} - Se empezo a subir imagen de comentario ${file}...`)
             return res.status(200).send({ message: 'El contenido de media se subio exitosamente.' })
         })
     },
--- middlewares/validateFileUpload.js
+++ middlewares/validateFileUpload.js
@@ -1,5 +1,25 @@
 'use strict'
+
+require('dotenv').config()
+const fs = require('fs')
 const multer = require('multer')
+const { v2: cloudinary, v2: { uploader } } = require('cloudinary')
+const streamifier = require('streamifier')
+
+let canWrite = false
+try {
+    fs.accessSync('./tmp', fs.constants.W_OK)
+    canWrite = true
+}
+catch (err) {
+    console.log('Local File Storage:', err.message)
+}
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -26,39 +46,69 @@ const fileFilter = (req, file, cb) => {
     const name = file.originalname
     const index = name.lastIndexOf('.')
     const fileExt = index > 0 ? name.substring(index + 1) : ''
-    if (!validExtensions.includes(fileExt.toLowerCase())) cb('La extension no es soportada.')
+    if (!validExtensions.includes(fileExt.toLowerCase())) return cb('La extension no es soportada.')
     cb(null, true)
 }
 const uploadProfileBanner = multer({ storage: storage, limits: limitProfileBanner, fileFilter })
 const uploadProfilePicture = multer({ storage: storage, limits: limitProfilePicture, fileFilter })
 const uploadMediaContent = multer({ storage: storage, limits: limitMediaContent, fileFilter })
 
+const uploadProfileBannerMemory = multer({ limits: limitProfileBanner, fileFilter })
+const uploadProfilePictureMemory = multer({ limits: limitProfilePicture, fileFilter })
+const uploadMediaContentMemory = multer({ limits: limitMediaContent, fileFilter })
+
+const streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+        const stream = uploader.upload_stream((err, res) => {
+            if (res) resolve(res)
+            else reject(err)
+        })
+        streamifier.createReadStream(req.file.buffer).pipe(stream)
+    })
+}
+
 const fileUploadValidation = {
     uploadSingleProfileBanner: (req, res, next) => {
-        const upload = uploadProfileBanner.single('profileBanner')
-        upload(req, res, (err) => {
+        const uploadSingle = canWrite ? uploadProfileBanner.single('profileBanner') : uploadProfileBannerMemory.single('profileBanner')
+        uploadSingle(req, res, async (err) => {
             if (err) return res.status(422).send({ message: err })
+            if (!canWrite) {
+                const result = await streamUpload(req)
+                req.cloudinary_url = result.secure_url
+            }
             next()
         })
     },
     uploadSingleProfilePicture: (req, res, next) => {
-        const upload = uploadProfilePicture.single('profilePicture')
-        upload(req, res, (err) => {
+        const uploadSingle = canWrite ? uploadProfilePicture.single('profilePicture') : uploadProfilePictureMemory.single('profilePicture')
+        uploadSingle(req, res, async (err) => {
             if (err) return res.status(422).send({ message: err })
+            if (!canWrite) {
+                const result = await streamUpload(req)
+                req.cloudinary_url = result.secure_url
+            }
             next()
         })
     },
     uploadSingleTweetImage: (req, res, next) => {
-        const upload = uploadMediaContent.single('tweetImage')
-        upload(req, res, (err) => {
+        const uploadSingle = canWrite ? uploadMediaContent.single('tweetImage') : uploadMediaContentMemory.single('tweetImage')
+        uploadSingle(req, res, async (err) => {
             if (err) return res.status(422).send({ message: err })
+            if (!canWrite) {
+                const result = await streamUpload(req)
+                req.cloudinary_url = result.secure_url
+            }
             next()
         })
     },
     uploadSingleCommentImage: (req, res, next) => {
-        const upload = uploadMediaContent.single('commentImage')
-        upload(req, res, (err) => {
+        const uploadSingle = canWrite ? uploadMediaContent.single('commentImage') : uploadMediaContentMemory.single('commentImage')
+        uploadSingle(req, res, async (err) => {
             if (err) return res.status(422).send({ message: err })
+            if (!canWrite) {
+                const result = await streamUpload(req)
+                req.cloudinary_url = result.secure_url
+            }
             next()
         })
     },
